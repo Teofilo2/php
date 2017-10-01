441def9f6e06a5c16deb3cdd70ea4666259a1c19
Teofilo Laurentino <ps04591@gmail.com>
2017-10-01 12:32:09 -0300

    resolvi conflito

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 	echo "Olá mundo 456";
-=======
-	echo "Olá mundo 123";
 
-	echo "Olá Teo";
->>>>>>> e9b6f0222bf021667e340fd3c3f2685d8a3a4a76
-
-	echo "Olá Udemy";
+	echo "Olá Udemy e Teo";
 ?>
